b44a2164a8613516ea298f5b331199fa61aaf137
lenayak <113600536+lenayak@users.noreply.github.com>
2022-12-23 13:31:58 +0400

    added lemmatization

--- main.py
+++ main.py
@@ -63,3 +63,65 @@ def cleanwords(words: str) -> str:
 def pos(word, morth=pymorphy2.MorphAnalyzer()):
     "return a likely part of speech for the *word*."""
     return morth.parse(word)[0].tag.POS
+
+
+def Lemmatize(datafraime: pd.DataFrame, column: str):
+    """lem"""
+    text_nomalized = str()
+    for i in range(0, len(datafraime.index)):
+        text = datafraime.iloc[i]
+        text = text[column]
+        words = text.split()
+        words = cleanwords(words)
+        for i in range(0,len(words)):
+            text_nomalized += words[i]
+            text_nomalized += ' '
+    m = Mystem()
+    lemmas = m.lemmatize(text_nomalized)
+    functors_pos = {'ADJF'}  
+    lemmas = [lemma for lemma in lemmas if pos(lemma) in functors_pos]
+    print(lemmas)
+    lemmas = cleanwords(lemmas)     
+    return lemmas
+    
+def LemmalizeClass(datafraime: pd.DataFrame, column: str, mark:str) -> str:
+    lemmas = Lemmatize(datafraime, column)
+    word_dict = Counter(lemmas)
+    word_dict = dict(word_dict) 
+    result = dict()
+    for key, value in word_dict.items():    
+        if value > 500:
+            result[key] = value
+    return result
+
+
+def Top10Lemmas(lemmatized: str) -> str:
+    pass
+
+
+if __name__ == '__main__':
+    print("-"*99)
+    columns = ["mark", "review", "num_of_words"]
+    dataset_path = get_path_to_dataset()
+    dataframe = get_datafraime(dataset_path)
+    num_of_words = count_of_words(dataframe, "review")
+    dataframe[columns[2]] = pd.Series(num_of_words)
+    dataframe[columns[2]] = pd.Series(num_of_words)
+    print(dataframe)
+    stat = dataframe[columns[2]].describe()
+    print(stat)
+    df_words_filtered = pd.DataFrame(dataframe[dataframe[columns[2]] <= 100])
+    print(df_words_filtered)
+    df_1 = pd.DataFrame(dataframe[dataframe[columns[0]] == '1'])
+    print(df_1)
+    stat_1 = df_1[columns[2]].describe()
+    print('\nДля оценки 1:\n')
+    print('Минимальное кол-во слов:', stat_1['min'])
+    print('Максимальное кол-во слов:', stat_1['max'])
+    print('Среднее кол-во слов:', stat_1['mean'])
+    lemmatized_class = LemmalizeClass(dataframe, columns[1], '1')
+    fig = pl.figure(figsize=(20,10))
+    ax = fig.add_subplot()
+    ax.bar(list(lemmatized_class.keys()), lemmatized_class.values(), color='g')
+    pl.show()
+    print("-"*99)
